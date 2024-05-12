178e2bd7621ab6db9866d5ff9728a595d9a96f21
mominur-emon <mominur.emon19@gmail.com>
2024-05-12 20:53:30 +0600

    completed

--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -1,132 +1,3 @@
-// import { NextResponse } from "next/server";
-
-// // Import Stripe with Secret Key
-// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-
-// const getActiveProduct = async () => {
-//   const checkProducts = await stripe.products.list();
-//   const availableProducts = checkProducts.data.filter(
-//     (product) => product.active === true
-//   );
-//   return availableProducts;
-// };
-// export const POST = async (request) => {
-//   try {
-//     const { products } = await request.json();
-//     const data = products;
-
-//     let activeProducts = await getActiveProduct();
-
-//     // Use map to create an array of promises
-//     const createProductPromises = data.map(async (product) => {
-//       const stripeProduct = activeProducts.find(
-//         (stripeProduct) =>
-//           stripeProduct?.name?.toLowerCase() === product?.name?.toLowerCase()
-//       );
-
-//       if (stripeProduct === undefined) {
-//         try {
-//           const prod = await stripe.products.create({
-//             name: product.name,
-//             default_price_data: {
-//               unit_amount: product.price * 100,
-//               currency: "usd",
-//             },
-//           });
-//           console.log("Created product:", prod);
-//         } catch (error) {
-//           console.error("Error creating product:", error);
-//         }
-//       }
-//     });
-
-//     // Wait for all promises to resolve
-//     await Promise.all(createProductPromises);
-
-//     return NextResponse.json({ url: "" });
-//   } catch (error) {
-//     console.error("Error:", error);
-//     return NextResponse.error(
-//       new Error("An error occurred while processing the request")
-//     );
-//   }
-// };
-
-// import { NextResponse } from "next/server";
-
-// // Import Stripe with Secret Key
-// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
-
-// const getActiveProduct = async () => {
-//   const checkProducts = await stripe.products.list();
-//   const availableProducts = checkProducts.data.filter(
-//     (product) => product.active === true
-//   );
-//   return availableProducts;
-// };
-// export const POST = async (request) => {
-//   try {
-//     const { products } = await request.json();
-//     const data = products;
-
-//     let activeProducts = await getActiveProduct();
-
-//     // Use map to create an array of promises
-//     const createProductPromises = data.map(async (product) => {
-//       const stripeProduct = activeProducts.find(
-//         (stripeProduct) =>
-//           stripeProduct?.name?.toLowerCase() === product?.name?.toLowerCase()
-//       );
-
-//       if (stripeProduct === undefined) {
-//         try {
-//           const prod = await stripe.products.create({
-//             name: product.name,
-//             default_price_data: {
-//               unit_amount: product.price * 100,
-//               currency: "usd",
-//             },
-//           });
-//           console.log("Created product:", prod);
-//         } catch (error) {
-//           console.error("Error creating product:", error);
-//         }
-//       }
-//     });
-
-//     // Wait for all promises to resolve
-//     await Promise.all(createProductPromises);
-
-//     activeProducts = await getActiveProduct();
-//     let stripeItems = [];
-//     for (const product of data) {
-//       const stripeProduct = activeProducts?.find(
-//         stripeProduct?.name?.toLowerCase() === product?.name?.toLowerCase()
-//       );
-
-//       if (stripeProduct) {
-//         stripeItems.push({
-//           price: stripeProduct?.default_price,
-//           quantity: product?.quantity,
-//         });
-//       }
-//     }
-//     const session = await stripe.checkout.sessions.create({
-//       line_items: stripeItems,
-//       mode: "payment",
-//       success_url: "http://localhost:3000/success",
-//       cancel_url: "http://localhost:3000/cancel",
-//     });
-
-//     return NextResponse.json({ url: session.url });
-//   } catch (error) {
-//     console.error("Error:", error);
-//     return NextResponse.error(
-//       new Error("An error occurred while processing the request")
-//     );
-//   }
-// };
-
 import { NextResponse } from "next/server";
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
@@ -169,27 +40,27 @@ export const POST = async (request) => {
   }
 
   //update product
-  activeProducts = await getActiveProduct();
-  let stripeItems = [];
+  // activeProducts = await getActiveProduct();
+  // let stripeItems = [];
 
-  for (const product of data) {
-    const stripeProduct = activeProducts?.find(
-      (prod) => prod?.name?.toLowerCase() == product?.name?.toLowerCase()
-    );
-    if (stripeProduct) {
-      stripeItems.push({
-        price: stripeProduct?.default_price,
-        quantity: product?.quantity,
-      });
-    }
-  }
+  // for (const product of data) {
+  //   const stripeProduct = activeProducts?.find(
+  //     (prod) => prod?.name?.toLowerCase() == product?.name?.toLowerCase()
+  //   );
+  //   if (stripeProduct) {
+  //     stripeItems.push({
+  //       price: stripeProduct?.default_price,
+  //       quantity: product?.quantity,
+  //     });
+  //   }
+  // }
 
-  const session = await stripe.checkout.sessions.create({
-    line_items: stripeItems,
-    mode: "payment",
-    success_url: "http://localhost:3000/success",
-    cancel_url: "http://localhost:3000/cancel",
-  });
+  // const session = await stripe.checkout.sessions.create({
+  //   line_items: stripeItems,
+  //   mode: "payment",
+  //   success_url: "http://localhost:3000/success",
+  //   cancel_url: "http://localhost:3000/cancel",
+  // });
 
   return NextResponse.json({ url: session.url });
 };
